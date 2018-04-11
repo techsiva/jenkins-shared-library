6dc1b8c918e18304b1b6c881a877137f35510cc4
Rubankumar Sathyamoorthy <rubankumar.sathyamoorthy@cognizant.com>
2018-04-11 12:32:14 +0000

    initial commit

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -10,31 +10,29 @@ def call(body) {
 		deleteDir()
 
 		try {
-			stages {
-				stage ('Compile Stage') {   
-					steps {
-			        	sh "echo 'building ${config.projectName} ...'"
-						withMaven(maven : 'maven_3_5_0') {
-							sh 'mvn clean compile'
-						}
+			stage ('Compile Stage') {   
+				steps {
+					sh "echo 'building ${config.projectName} ...'"
+					withMaven(maven : 'maven_3_5_0') {
+						sh 'mvn clean compile'
 					}
 				}
-				stage ('Testing Stage') {
-					steps {
-						withMaven(maven : 'maven_3_5_0') {
-							sh 'mvn test'
-						}
+			}
+			stage ('Testing Stage') {
+				steps {
+					withMaven(maven : 'maven_3_5_0') {
+						sh 'mvn test'
 					}
 				}
-				stage ('Deployment Stage') {
-					steps {
-						withMaven(maven : 'maven_3_5_0') {
-							sh 'mvn deploy'
-						}
+			}
+			stage ('Deployment Stage') {
+				steps {
+					withMaven(maven : 'maven_3_5_0') {
+						sh 'mvn deploy'
 					}
 				}
-			}       
-				post {
+			}			       
+			post {
 				success {
 					mail to:"ruban.yuvaraj@gmail.com", subject: "SUCCESSFUL: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'", body: "SUCCESSFUL: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
 				}
