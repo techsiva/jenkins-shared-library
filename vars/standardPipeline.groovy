4aa04f49053ee2f55d20091604582563eba6e2d9
Rubankumar Sathyamoorthy <rubankumar.sathyamoorthy@cognizant.com>
2018-04-11 12:30:14 +0000

    initial commit

--- vars/standardPipeline.groovy
+++ vars/standardPipeline.groovy
@@ -1,38 +1,50 @@
-    def call(body) {
+def call(body) {
 
-        def config = [:]
-        body.resolveStrategy = Closure.DELEGATE_FIRST
-        body.delegate = config
-        body()
+	def config = [:]
+	body.resolveStrategy = Closure.DELEGATE_FIRST
+	body.delegate = config
+	body()
 
-        node {
-            // Clean workspace before doing anything
-            deleteDir()
+	node {
+		// Clean workspace before doing anything
+		deleteDir()
 
-            try {
-                stage ('Clone') {
-                    checkout scm
-                }
-                stage ('Build') {
-                    sh "echo 'building ${config.projectName} ...'"
-                }
-                stage ('Tests') {
-                    parallel 'static': {
-                        sh "echo 'shell scripts to run static tests...'"
-                    },
-                    'unit': {
-                        sh "echo 'shell scripts to run unit tests...'"
-                    },
-                    'integration': {
-                        sh "echo 'shell scripts to run integration tests...'"
-                    }
-                }
-                stage ('Deploy') {
-                    sh "echo 'deploying to server ${config.serverDomain}...'"
-                }
-            } catch (err) {
-                currentBuild.result = 'FAILED'
-                throw err
-            }
-        }
-    }
+		try {
+			stages {
+				stage ('Compile Stage') {   
+					steps {
+			        	sh "echo 'building ${config.projectName} ...'"
+						withMaven(maven : 'maven_3_5_0') {
+							sh 'mvn clean compile'
+						}
+					}
+				}
+				stage ('Testing Stage') {
+					steps {
+						withMaven(maven : 'maven_3_5_0') {
+							sh 'mvn test'
+						}
+					}
+				}
+				stage ('Deployment Stage') {
+					steps {
+						withMaven(maven : 'maven_3_5_0') {
+							sh 'mvn deploy'
+						}
+					}
+				}
+			}       
+				post {
+				success {
+					mail to:"ruban.yuvaraj@gmail.com", subject: "SUCCESSFUL: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'", body: "SUCCESSFUL: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
+				}
+				failure {
+					mail to:"ruban.yuvaraj@gmail.com", subject: "FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'", body: "FAILED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
+				}
+			}
+		} catch (err) {
+			currentBuild.result = 'FAILED'
+			throw err
+		}
+	}
+}
